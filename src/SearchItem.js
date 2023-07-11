8da01a2c1978b7223114ed788049f243bd1b7cd7
Akm208 <akmalmuhammad554@gmail.com>
2023-07-11 09:10:46 +0500

    Controlled Components

---
+++ src/SearchItem.js
@@ -0,0 +1,23 @@
+
+
+const SearchItem = ({search,setSearch}) => {
+    
+  return (
+    <form className='searchForm' onSubmit={(e)=>e.preventDefault()}>
+      <label htmlFor='search'>Search</label>
+      <input 
+        id='search'
+        type='text'
+       
+        role='checkbox'
+        placeholder='Search Items'
+        value={search}
+        onChange={(e)=>setSearch(e.target.value)}
+       
+
+      />
+    </form>
+  )
+}
+
+export default SearchItem
